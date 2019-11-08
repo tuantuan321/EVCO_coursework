f6de247b4c75e9a42df00689035e72c3d724e22b
Zheyu <me@chunnitu.com>
2019-11-08 18:15:21 +0000

    Fixed snakePlay_Program running error on Python3

--- snakeProblem.py
+++ snakeProblem.py
@@ -17,61 +17,61 @@ NFOOD = 1
 
 # This class can be used to create a basic player object (snake agent)
 class SnakePlayer(list):
-	global S_RIGHT, S_LEFT, S_UP, S_DOWN
-	global XSIZE, YSIZE
+    global S_RIGHT, S_LEFT, S_UP, S_DOWN
+    global XSIZE, YSIZE
 
-	def __init__(self):
-		self.direction = S_RIGHT
-		self.body = [ [4,10], [4,9], [4,8], [4,7], [4,6], [4,5], [4,4], [4,3], [4,2], [4,1],[4,0] ]
-		self.score = 0
-		self.ahead = []
-		self.food = []
+    def __init__(self):
+        self.direction = S_RIGHT
+        self.body = [ [4,10], [4,9], [4,8], [4,7], [4,6], [4,5], [4,4], [4,3], [4,2], [4,1],[4,0] ]
+        self.score = 0
+        self.ahead = []
+        self.food = []
 
-	def _reset(self):
-		self.direction = S_RIGHT
-		self.body[:] = [ [4,10], [4,9], [4,8], [4,7], [4,6], [4,5], [4,4], [4,3], [4,2], [4,1],[4,0] ]
-		self.score = 0
-		self.ahead = []
-		self.food = []
+    def _reset(self):
+        self.direction = S_RIGHT
+        self.body[:] = [ [4,10], [4,9], [4,8], [4,7], [4,6], [4,5], [4,4], [4,3], [4,2], [4,1],[4,0] ]
+        self.score = 0
+        self.ahead = []
+        self.food = []
 
-	def getAheadLocation(self):
-		self.ahead = [ self.body[0][0] + (self.direction == S_DOWN and 1) + (self.direction == S_UP and -1), self.body[0][1] + (self.direction == S_LEFT and -1) + (self.direction == S_RIGHT and 1)] 
+    def getAheadLocation(self):
+        self.ahead = [ self.body[0][0] + (self.direction == S_DOWN and 1) + (self.direction == S_UP and -1), self.body[0][1] + (self.direction == S_LEFT and -1) + (self.direction == S_RIGHT and 1)] 
 
-	def updatePosition(self):
-		self.getAheadLocation()
-		self.body.insert(0, self.ahead )
+    def updatePosition(self):
+        self.getAheadLocation()
+        self.body.insert(0, self.ahead )
 
 	## You are free to define more sensing options to the snake
 
-	def changeDirectionUp(self):
-		self.direction = S_UP
+    def changeDirectionUp(self):
+        self.direction = S_UP
 
-	def changeDirectionRight(self):
-		self.direction = S_RIGHT
+    def changeDirectionRight(self):
+        self.direction = S_RIGHT
 
-	def changeDirectionDown(self):
-		self.direction = S_DOWN
+    def changeDirectionDown(self):
+        self.direction = S_DOWN
 
-	def changeDirectionLeft(self):
-		self.direction = S_LEFT
+    def changeDirectionLeft(self):
+        self.direction = S_LEFT
 
-	def snakeHasCollided(self):
-		self.hit = False
-		if self.body[0][0] == 0 or self.body[0][0] == (YSIZE-1) or self.body[0][1] == 0 or self.body[0][1] == (XSIZE-1): self.hit = True
-		if self.body[0] in self.body[1:]: self.hit = True
-		return( self.hit )
+    def snakeHasCollided(self):
+        self.hit = False
+        if self.body[0][0] == 0 or self.body[0][0] == (YSIZE-1) or self.body[0][1] == 0 or self.body[0][1] == (XSIZE-1): self.hit = True
+        if self.body[0] in self.body[1:]: self.hit = True
+        return( self.hit )
 
-	def sense_wall_ahead(self):
-		self.getAheadLocation()
-		return( self.ahead[0] == 0 or self.ahead[0] == (YSIZE-1) or self.ahead[1] == 0 or self.ahead[1] == (XSIZE-1) )
+    def sense_wall_ahead(self):
+        self.getAheadLocation()
+        return( self.ahead[0] == 0 or self.ahead[0] == (YSIZE-1) or self.ahead[1] == 0 or self.ahead[1] == (XSIZE-1) )
 
-	def sense_food_ahead(self):
-		self.getAheadLocation()
-		return self.ahead in self.food
+    def sense_food_ahead(self):
+        self.getAheadLocation()
+        return self.ahead in self.food
 
-	def sense_tail_ahead(self):
-		self.getAheadLocation()
-		return self.ahead in self.body
+    def sense_tail_ahead(self):
+        self.getAheadLocation()
+        return self.ahead in self.body
 
 # This function places a food item in the environment
 def placeFood(snake):
@@ -83,107 +83,103 @@ def placeFood(snake):
 	snake.food = food  # let the snake know where the food is
 	return( food )
 
-
 snake = SnakePlayer()
-
 
 # This outline function is the same as runGame (see below). However,
 # it displays the game graphically and thus runs slower
 # This function is designed for you to be able to view and assess
 # your strategies, rather than use during the course of evolution
 def displayStrategyRun():
-	global snake
-	global pset
+    global snake
+    global pset
 
-	routine = gp.compile(individual, pset)
+    routine = gp.compile(individual, pset)
 
-	curses.initscr()
-	win = curses.newwin(YSIZE, XSIZE, 0, 0)
-	win.keypad(1)
-	curses.noecho()
-	curses.curs_set(0)
-	win.border(0)
-	win.nodelay(1)
-	win.timeout(120)
+    curses.initscr()
+    win = curses.newwin(YSIZE, XSIZE, 0, 0)
+    win.keypad(1)
+    curses.noecho()
+    curses.curs_set(0)
+    win.border(0)
+    win.nodelay(1)
+    win.timeout(120)
 
-	snake._reset()
-	food = placeFood(snake)
+    snake._reset()
+    food = placeFood(snake)
 
-	for f in food:
-		win.addch(f[0], f[1], '@')
+    for f in food:
+        win.addch(f[0], f[1], '@')
 
-	timer = 0
-	collided = False
-	while not collided and not timer == ((2*XSIZE) * YSIZE):
+    timer = 0
+    collided = False
+    while not collided and not timer == ((2*XSIZE) * YSIZE):
 
 		# Set up the display
-		win.border(0)
-		win.addstr(0, 2, 'Score : ' + str(snake.score) + ' ')
- 		win.getch()
+        win.border(0)
+        win.addstr(0, 2, 'Score : ' + str(snake.score) + ' ')
+        win.getch()
 
 		## EXECUTE THE SNAKE'S BEHAVIOUR HERE ##
+        snake.updatePosition()
 
-		snake.updatePosition()
+        if snake.body[0] in food:
+            snake.score += 1
+            for f in food: win.addch(f[0], f[1], ' ')
+            food = placeFood(snake)
+            for f in food: win.addch(f[0], f[1], '@')
+            timer = 0
+        else:
+            last = snake.body.pop()
+            win.addch(last[0], last[1], ' ')
+            timer += 1 # timesteps since last eaten
+        win.addch(snake.body[0][0], snake.body[0][1], 'o')
 
-		if snake.body[0] in food:
-			snake.score += 1
-			for f in food: win.addch(f[0], f[1], ' ')
-			food = placeFood(snake)
-			for f in food: win.addch(f[0], f[1], '@')
-			timer = 0
-		else:
-			last = snake.body.pop()
-			win.addch(last[0], last[1], ' ')
-			timer += 1 # timesteps since last eaten
-		win.addch(snake.body[0][0], snake.body[0][1], 'o')
+        collided = snake.snakeHasCollided()
+        hitBounds = (timer == ((2*XSIZE) * YSIZE))
 
-		collided = snake.snakeHasCollided()
-		hitBounds = (timer == ((2*XSIZE) * YSIZE))
+    curses.endwin()
 
-	curses.endwin()
+    print(collided)
+    print(hitBounds)
+    raw_input("Press to continue...")
 
-	print collided
-	print hitBounds
-	raw_input("Press to continue...")
-
-	return snake.score,
+    return snake.score,
 
 
 # This outline function provides partial code for running the game with an evolved agent
 # There is no graphical output, and it runs rapidly, making it ideal for
 # you need to modify it for running your agents through the game for evaluation
 # which will depend on what type of EA you have used, etc.
-# Feel free to make any necessary modifications to this section.
 def runGame():
-	global snake
 
-	totalScore = 0
+    global snake
 
-	snake._reset()
-	food = placeFood(snake)
-	timer = 0
-	while not snake.snakeHasCollided() and not timer == XSIZE * YSIZE:
+    totalScore = 0
+
+    snake._reset()
+    food = placeFood(snake)
+    timer = 0
+    while not snake.snakeHasCollided() and not timer == XSIZE * YSIZE:
 
 		## EXECUTE THE SNAKE'S BEHAVIOUR HERE ##
 
-		snake.updatePosition()
+        snake.updatePosition()
 
-		if snake.body[0] in food:
-			snake.score += 1
-			food = placeFood(snake)
-			timer = 0
-		else:    
-			snake.body.pop()
-			timer += 1 # timesteps since last eaten
+        if snake.body[0] in food:
+            snake.score += 1
+            food = placeFood(snake)
+            timer = 0
+        else:
+            snake.body.pop()
+            timer += 1 # timesteps since last eaten
 
-		totalScore += snake.score
+        totalScore += snake.score
 
-	return totalScore,
-
+    return totalScore,
 
 def main():
-	global snake
-	global pset
+    global snake
+    global pset
 
 	## THIS IS WHERE YOUR CORE EVOLUTIONARY ALGORITHM WILL GO #
 
